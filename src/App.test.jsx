6a03bbe4a6b8fc2bb8e1e86d8114294811cda638
Teoslip <tjachiet@gmail.com>
2026-01-28 00:35:11 +0100

    add integration test of adding a cart

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,26 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from "@testing-library/react";
-import App from "./App";
-import { MemoryRouter } from 'react-router';
+import userEvent from '@testing-library/user-event';
+import { createMemoryRouter, RouterProvider } from 'react-router'
+import routes from './routes';
 
 describe('App component', () => {
-  it('dummy test', () => {
-    render(<MemoryRouter>
-      <App/>
-    </MemoryRouter>);
-    expect(true).toBe(true);
+  it('adding an item updates cart', async () => {
+    const user = userEvent.setup();
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ['/store'],
+    })
+
+    render(<RouterProvider router={router}/>)
+
+    const addToCartButton = screen.getAllByRole('button', { name: /add to cart/i })[0];
+    const cartLink = screen.getByRole('link', { name: /cart/i })
+
+    await user.click(addToCartButton);
+    await user.click(cartLink);
+
+    expect(await screen.findByText('vest')).toBeInTheDocument();
+    expect(screen.getByText(/total/i)).toHaveTextContent('€')
   });
 });
