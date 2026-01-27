56cbf622111264bdd331e98d77a5b61d4df9eb0b
Teoslip <tjachiet@gmail.com>
2026-01-27 23:47:03 +0100

    add function calls test to Cart Item component

--- src/components/CartItem/CartItem.test.jsx
+++ src/components/CartItem/CartItem.test.jsx
@@ -2,14 +2,19 @@ import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import CartItem from "./CartItem";
+import userEvent from "@testing-library/user-event";
 
-const mockItem = {id: 1, imageUrl: '#', name: 'vest', price: 40, quantity: 1 };
+const mockItem = { id: 1, imageUrl: '#', name: 'vest', price: 40, quantity: 1 };
+const handleChangeQuantity = vi.fn();
+const handleIncreaseQuantity = vi.fn();
+const handleDecreaseQuantity = vi.fn();
+const handleRemove = vi.fn();
 
 describe("CartItem Component", () => {
     it("renders all sections of the item", () => {
         render(<MemoryRouter>
             <CartItem
-                key={1} 
+                key={1}
                 item={mockItem}
             />
         </MemoryRouter>);
@@ -30,4 +35,37 @@ describe("CartItem Component", () => {
         //Price
         expect(screen.getByText(/€/i)).toBeInTheDocument();
     });
+
+    it("calls the functions on clicks", async () => {
+        render(<MemoryRouter>
+            <CartItem
+                key={1}
+                item={mockItem}
+                handleIncreaseQuantity={handleIncreaseQuantity}
+                handleDecreaseQuantity={handleDecreaseQuantity}
+                handleChangeQuantity={handleChangeQuantity}
+                handleRemove={handleRemove}
+            />
+        </MemoryRouter>);
+
+        const user = userEvent.setup();
+
+        const inputQuantity = screen.getByRole('textbox');
+        const plusButton = screen.getByRole('button', { name: '+' });
+        const minusButton = screen.getByRole('button', { name: '-' });
+        const removeButton = screen.getByRole('button', { name: 'Remove' });
+
+        await user.click(plusButton);
+        expect(handleIncreaseQuantity).toHaveBeenCalledTimes(1);
+
+        await user.click(minusButton);
+        expect(handleDecreaseQuantity).toHaveBeenCalledTimes(1);
+
+        await user.click(removeButton);
+        expect(handleRemove).toHaveBeenCalledTimes(1);
+
+        await user.type(inputQuantity, "11");
+        expect(handleChangeQuantity).toHaveBeenCalledTimes(2);
+
+    })
 });
