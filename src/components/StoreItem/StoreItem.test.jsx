6dd912dc18fbcb212fb8f541e27c2a9f68395d59
Teoslip <tjachiet@gmail.com>
2026-01-27 23:43:12 +0100

    add function calls test to StoreItem component

--- src/components/StoreItem/StoreItem.test.jsx
+++ src/components/StoreItem/StoreItem.test.jsx
@@ -1,16 +1,25 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import StoreItem from "./StoreItem";
+import userEvent from "@testing-library/user-event";
 
 const mockItem = { id: 1, imageUrl: '#', name: 'vest', price: 40, quantity: 1 };
+const handleChangeQuantity = vi.fn();
+const handleIncreaseQuantity = vi.fn();
+const handleDecreaseQuantity = vi.fn();
+const handleAddToCart = vi.fn();
 
 describe("StoreItem Component", () => {
     it("renders all sections of the item", () => {
         render(<MemoryRouter>
             <StoreItem
-                key={1} 
+                key={1}
                 item={mockItem}
+                handleChangeQuantity={handleChangeQuantity}
+                handleIncreaseQuantity={handleIncreaseQuantity}
+                handleDecreaseQuantity={handleDecreaseQuantity}
+                handleAddToCart={handleAddToCart}
             />
         </MemoryRouter>);
 
@@ -27,4 +36,37 @@ describe("StoreItem Component", () => {
         //Add to cart
         expect(screen.getByRole('button', { name: 'Add to Cart' })).toBeInTheDocument()
     });
+
+    it("calls the functions on clicks", async () => {
+        render(<MemoryRouter>
+            <StoreItem
+                key={1}
+                item={mockItem}
+                handleChangeQuantity={handleChangeQuantity}
+                handleIncreaseQuantity={handleIncreaseQuantity}
+                handleDecreaseQuantity={handleDecreaseQuantity}
+                handleAddToCart={handleAddToCart}
+            />
+        </MemoryRouter>);
+
+        const user = userEvent.setup();
+
+        const inputQuantity = screen.getByRole('textbox');
+        const plusButton = screen.getByRole('button', {name: '+'});
+        const minusButton = screen.getByRole('button', {name: '-'});
+        const addToCartButton = screen.getByRole('button', {name: 'Add to Cart'});
+
+        await user.click(plusButton);
+        expect(handleIncreaseQuantity).toHaveBeenCalledTimes(1);
+
+        await user.click(minusButton);
+        expect(handleDecreaseQuantity).toHaveBeenCalledTimes(1);
+
+        await user.click(addToCartButton);
+        expect(handleAddToCart).toHaveBeenCalledTimes(1);
+
+        await user.type(inputQuantity, "11");
+        expect(handleChangeQuantity).toHaveBeenCalledTimes(2);
+
+    })
 });
