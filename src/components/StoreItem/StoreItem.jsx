17ea063fe6a36095490e2d08fb57bf4b0a03b646
Teoslip <tjachiet@gmail.com>
2026-01-31 20:51:19 +0100

    style storeItem

--- src/components/StoreItem/StoreItem.jsx
+++ src/components/StoreItem/StoreItem.jsx
@@ -3,18 +3,20 @@ import styles from './StoreItem.module.css';
 const StoreItem = ({ item, handleChangeQuantity, handleIncreaseQuantity, handleDecreaseQuantity, handleAddToCart }) => {
     return (
         <>
-            <div className='storeItem'>
+            <div className={styles.storeItem}>
                 <img src={item.imageUrl} alt="item image" />
-                <div className="item-name">{item.name}</div>
-                <div className="item-price">{item.price} €</div>
-                <div className="counter">
-                    <button onClick={() => handleDecreaseQuantity(item.id, item.quantity)}>-</button>
-                    <input type="text" value={item.quantity} onChange={(e) => handleChangeQuantity(item.id, e.target.value)} />
-                    <button onClick={() => handleIncreaseQuantity(item.id, item.quantity)}>+</button>
+                <div className={styles.info}>
+                    <div className={styles.name}>{item.name}</div>
+                    <div className="item-price">{item.price} €</div>
+                    <div className={styles.counter}>
+                        <button onClick={() => handleDecreaseQuantity(item.id, item.quantity)}>-</button>
+                        <input type="text" value={item.quantity} onChange={(e) => handleChangeQuantity(item.id, e.target.value)} />
+                        <button onClick={() => handleIncreaseQuantity(item.id, item.quantity)}>+</button>
+                    </div>
+                    <button className={styles.addItem} onClick={() => handleAddToCart(item.id)}>
+                        Add to Cart
+                    </button>
                 </div>
-                <button className="add-item" onClick={() => handleAddToCart(item.id)}>
-                    Add to Cart
-                </button>
             </div>
         </>
 
