ff1a12fc041ea3fc0925d32a5354c0e5aea46f26
Teoslip <tjachiet@gmail.com>
2026-01-31 11:59:24 +0100

    style homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,20 @@
 import styles from './Home.module.css';
+import { Link } from 'react-router';
+
 
 const Home = () => {
     return (
         <>
             <section>
-                <h2><span className="welcome">WELCOME</span> TO RANDSHOP</h2>
-                <p>Here you can shop random items that have no correlation between them in any way.
-                    Prices are kind of random and illustration images are ugly but fear not,
-                    you will not spend any real dollars on this shop as it has no backend... </p>
-                <button className="shop-now">Shop Now !</button>
+                <div className={styles.text}>
+                    <h2><div className={styles.welcome}>WELCOME</div> TO RANDSHOP</h2>
+                    <p>Here you can shop random items that have no correlation between them in any way.
+                        Prices are kind of random and illustration images are ugly but fear not,
+                        you will not spend any real dollars on this shop as it has no backend... </p>
+                    <Link to='/store' className={styles.now}>SHOP NOW !</Link>
+                </div>
+
+                <img src="/src/assets/images/shopping-illustration.jpg" alt="phone shopping" />
             </section>
         </>
     )
