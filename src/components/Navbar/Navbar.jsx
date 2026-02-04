27c0a3156cc03f423927d0cef61b7a9d29178869
Teoslip <tjachiet@gmail.com>
2026-02-04 11:42:32 +0100

    responsive home page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ({ cartItems }) => {
         <>
             <header>
                 <h1>RANDSHOP</h1>
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-menu-icon lucide-menu"><path d="M4 5h16" /><path d="M4 12h16" /><path d="M4 19h16" /></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M4 5h16" /><path d="M4 12h16" /><path d="M4 19h16" /></svg>
                 <ul>
                     <li><Link to="/home">HOME</Link></li>
                     <li><Link to="/store">STORE</Link></li>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,19 +4,17 @@ import { Link } from 'react-router';
 
 const Home = () => {
     return (
-        <>
-            <section>
-                <div className={styles.text}>
-                    <h2><div className={styles.welcome}>WELCOME</div> TO RANDSHOP</h2>
-                    <p className={styles.intro}>Here you can shop random items that have no correlation between them in any way.
-                        Prices are kind of random and illustration images are ugly but fear not,
-                        you will not spend any real dollars on this shop as it has no backend... </p>
-                    <Link to='/store' className={styles.now}>SHOP NOW !</Link>
-                </div>
+        <main>
+            <div className={styles.text}>
+                <h2><div className={styles.welcome}>WELCOME</div> TO RANDSHOP</h2>
+                <p className={styles.intro}>Here you can shop random items that have no correlation between them in any way.
+                    Prices are kind of random and illustration images are ugly but fear not,
+                    you will not spend any real dollars on this shop as it has no backend... </p>
+                <Link to='/store' className={styles.now}>SHOP NOW !</Link>
+            </div>
 
-                <img src="/src/assets/images/shopping-illustration.jpg" alt="phone shopping" />
-            </section>
-        </>
+            <img src="/src/assets/images/shopping-illustration.jpg" alt="phone shopping" />
+        </main>
     )
 }
 
